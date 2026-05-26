460d21a49084faa7929c4f4580c765ea32334568
juandavid-dotcom <davidrriddikc@gmail.com>
2026-05-26 19:36:33 -0400

    first exercise completed

---
+++ HomeWork1/Variables/Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+int integer = 42;
+
+double decimalNumber = 3.14159;
+
+float floatingNumber = 2.11f;
+
+char character = 'A';
+
+string text = "Juan David ";
+
+bool booleanValue = true;
+
+long longNumber = 9876543210L;
+
+byte byteValue = 255;
+
+
+Console.WriteLine($"int  :    {integer}");
+Console.WriteLine($"double :  {decimalNumber}");
+Console.WriteLine($"float :  {floatingNumber}");
+Console.WriteLine($"char   :  {character}");
+Console.WriteLine($"string :  {text}");
+Console.WriteLine($"bool   :  {booleanValue}");
+Console.WriteLine($"long   :  {longNumber}");
+Console.WriteLine($"byte   :  {byteValue}");
+
+Console.ReadKey();
